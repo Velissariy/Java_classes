c62edfdc50cfa7445114fd47744e58ca7d9103c6
Juno <polya.alexei@yandex.ru>
2023-06-05 12:26:21 +0300

    ListNode method is done

--- RedBlackTree/src/ListNode.java
+++ RedBlackTree/src/ListNode.java
@@ -1,12 +1,9 @@
-public class SIngleLinkedList {
+public class ListNode {
+    int val;
+    ListNode next;
 
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int x) {
-            val = x;
-        }
+    ListNode(int x) {
+        val = x;
     }
 
     public ListNode reverseList(ListNode head) {
@@ -23,32 +20,34 @@ public class SIngleLinkedList {
 
         return prev;
     }
-
-
-    ListNode head = new ListNode(1);
-    head.next =new
-
-    ListNode(2);
-
-    head.next.next =new
-
-    ListNode(3);
-
-    head.next.next.next =new
-
-    ListNode(4);
-
-    head.next.next.next.next =new
-
-    ListNode(5);
-
-    ListNode reversedHead = reverseList(head);
-
-// Результат: 5 -> 4 -> 3 -> 2 -> 1
-        while(reversedHead !=null)
-
-    {
-        System.out.print(reversedHead.val + " ");
-        reversedHead = reversedHead.next;
-    }
 }
+
+
+//    ListNode head = new ListNode(1);
+//
+//    head.next =new
+//
+//    ListNode(2);
+//
+//    head.next.next =new
+//
+//    ListNode(3);
+//
+//    head.next.next.next =new
+//
+//    ListNode(4);
+//
+//    head.next.next.next.next =new
+//
+//    ListNode(5);
+//
+//    ListNode reversedHead = reverseList(head);
+//
+//
+//        while(reversedHead !=null)
+//
+//    {
+//        System.out.print(reversedHead.val + " ");
+//        reversedHead = reversedHead.next;
+//    }
+//}
--- RedBlackTree/src/RBTRee.java
+++ RedBlackTree/src/RBTRee.java
@@ -1,91 +1,84 @@
 public class RBTRee {
-    public class RedBlackTree {
-        private static final boolean RED = true, BLACK = false;
+
+
+        private static final boolean RED = true;
+        private static final boolean BLACK = false;
+
 
         private class Node {
-            private int value;
-            private Node left, right, parent;
-            private boolean color;
+            int key;
+            int value;
+            Node left, right;
+            boolean color;
 
-            public Node(int value) {
+            Node(int key, int value, boolean color) {
+                this.key = key;
                 this.value = value;
-                this.color = RED;
+                this.color = color;
             }
         }
 
         private Node root;
 
-        public void insert(int value) {
-            root = insert(root, value);
+        private boolean isRed (Node x){
+            if (x == null) return false;
+            return x.color == RED;
+        }
+
+        private Node rotateLeft (Node h){
+            Node x = h.right;
+            h.right = x.left;
+            x.left = h;
+            x.color = h.color;
+            h.color = RED;
+            return x;
+        }
+
+        private Node rotateRight (Node h){
+            Node x = h.left;
+            h.left = x.right;
+            x.right = h;
+            x.color = h.color;
+            h.color = RED;
+            return x;
+        }
+
+        private void flipColors (Node h){
+            h.color = RED;
+            h.left.color = BLACK;
+            h.right.color = BLACK;
+        }
+
+        public void put ( int key, int value){
+            root = put(root, key, value);
             root.color = BLACK;
         }
 
-        private Node insert(Node node, int value) {
-            if (node == null) {
-                return new Node(value);
-            }
+        private Node put (Node h,int key, int value){
+            if (h == null) return new Node(key, value, RED);
+            if (key < h.key) h.left = put(h.left, key, value);
+            else if (key > h.key) h.right = put(h.right, key, value);
+            else h.value = value;
 
-            if (value < node.value) {
-                node.left = insert(node.left, value);
-                node.left.parent = node;
-            } else if (value > node.value) {
-                {
-                    node.right = insert(node.right, value);
-                    node.right.parent = node;
-                } else {
-                    // Duplicates not allowed
-                    return node;
-                }
+            if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
+            if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+            if (isRed(h.left) && isRed(h.right)) flipColors(h);
 
-                if (isRed(node.right) && !isRed(node.left)) {
-                    node = rotateLeft(node);
-                }
-                if (isRed(node.left) && isRed(node.left.left)) {
-                    node = rotateRight(node);
-                }
-                if (isRed(node.left) && isRed(node.right)) {
-                    flipColors(node);
-                }
-
-                return node;
-            }
-
-            private boolean isRed(Node x) {
-                if (x == null) return false;
-                return x.color == RED;
-            }
-
-            private Node rotateLeft(Node node) {
-                Node temp = node.right;
-                node.right = temp.left;
-                if (temp.left != null) {
-                    temp.left.parent = node;
-                }
-                temp.parent = node.parent;
-                temp.left = node;
-                node.parent = temp;
-                temp.color = node.color;
-                node.color = RED;
-                return temp;
-            }
-
-            private Node rotateRight(Node node) {
-                Node temp = node.left;
-                node.left = temp.right;
-                if (temp.right != null) {
-                    temp.right.parent = node;
-                }
-                temp.parent = node.parent;
-                temp.right = node;
-                node.parent = temp;
-                temp.color = node.color;
-                node.color = RED;
-                return temp;
-            }
-
-            private void flipColors(Node node) {
-                node.color = RED;
-                node.left.color = BLACK;
-                node.right.color = BLACK;
-            }
+            return h;
         }
+    }
+
+//    RBTRee tree = new RBTRee();
+//
+//    tree.put(1, 10);
+//    tree.put(2, 20);
+//    tree.put(3, 30);
+//    tree.put(4, 40);
+//    tree.put(5, 50);
+//
+//    // Вывод дерева в консоль
+//        System.out.println(tree.root.key); // 2
+//        System.out.println(tree.root.left.key); // 1
+//        System.out.println(tree.root.right.key); // 4
+//        System.out.println(tree.root.right.left.key); // 3
+//        System.out.println(tree.root.right.right.key); // 5
