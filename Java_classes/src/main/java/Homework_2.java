b5a837fb3b4333db9268683a6ca688ca507f9a3b
Juno <polya.alexei@yandex.ru>
2023-06-12 10:00:15 +0300

    Homework_2 is done.

---
+++ Java_classes/src/main/java/Homework_2.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+
+//Задание
+// Дана строка sql-запроса "select * from students where ".
+// Сформируйте часть WHERE этого запроса, используя StringBuilder.
+// Данные для фильтрации приведены ниже в виде json-строки.
+
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+//
+// Дополнительные задания
+// Дана json-строка (можно сохранить в файл и читать из файла)
+// [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},
+// {"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+// Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
+// Пример вывода:
+// Студент Иванов получил 5 по предмету Математика.
+// Студент Петрова получил 4 по предмету Информатика.
+// Студент Краснов получил 5 по предмету Физика.
+//
+// *Сравнить время выполнения замены символа "а" на "А" любой строки содержащей >1000 символов средствами String и StringBuilder.
+
+import java.util.Scanner;
+
+public class Homework_2 {
+    public static void main(String[] args) {
+        String query = "select * from students where ";
+        Map<String, Object> filters = new HashMap<>();
+        filters.put("name", "Ivanov");
+        filters.put("country", "Russia");
+        filters.put("city", "Moscow");
+        filters.put("age", null);
+        StringBuilder whereClause = new StringBuilder();
+
+        for (Map.Entry<String, Object> entry : filters.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+
+            if (value != null && !value.equals("null")) {
+                if (whereClause.length() > 0) {
+                    whereClause.append(", ");
+                }
+                whereClause.append(key).append("=").append(value);
+            }
+        }
+        String sql = query + whereClause.toString();
+        System.out.println(sql);
+    }
+}
+        //select * from students where name=Ivanov, country=Russia, city=Moscow, age=null
+
+
